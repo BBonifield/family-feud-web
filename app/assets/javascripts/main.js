b4f28a3900a74772864b0a430aa6697c4af14706
nicovalencia <hello@nico.io>
2012-09-19 19:51:28 -0600

    Change notice.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,7 +3,40 @@
   var app = {};
 
   app.init = function() {
-    console.log('hi');
+    app.createGame();
+  };
+
+  app.createGame = function() {
+    $.ajax({
+      url: "/game/create",
+      type: "post",
+      dataType: "json",
+      success: function(resp) {
+        console.log(resp);
+      }
+    });
+  };
+
+  app.setPlayers = function(player1, player2) {
+
+
+  };
+
+
+
+
+
+
+
+
+
+
+  app.changeNotice = function(text) {
+    var $notice = $('#notice');
+    $notice.fadeOut(function(){
+      $notice.html(text);
+      $notice.fadeIn();
+    });
   };
 
   window.app = app;
