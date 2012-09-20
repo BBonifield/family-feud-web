a41993cdb69b3da75cfd23edbdc60b1e3300aa59
Bob Bonifield <bobbonifield@gmail.com>
2012-09-19 18:18:38 -0600

    Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FamilyFeudWeb::Application.config.secret_token = 'cf9428585fac4263f7f4fd6abaabc5226c21ed2d9b183538aea00d341949199a3fc7a3255a3bf14c936908cb762e68fa30c8d381838eb36f7076c02fa7583737'
